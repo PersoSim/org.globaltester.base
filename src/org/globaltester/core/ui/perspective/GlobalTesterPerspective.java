feb7edc8b5ff2bfc2a4369e2f6ab1d0f80ef88e5
Alexander May <alexander.may@hjp-consulting.com>
2011-04-11 09:08:34 +0000

    added (empty) GlobalTester Navigator View
    git-svn-id: http://subversion.globaltester.org/repo/globaltester/org.globaltester.core.ui/trunk@75 cd90bb66-5c64-11e0-9e8e-9b0b1919da2d

--- src/org/globaltester/core/ui/perspective/GlobalTesterPerspective.java
+++ src/org/globaltester/core/ui/perspective/GlobalTesterPerspective.java
@@ -6,5 +6,9 @@ import org.eclipse.ui.IPerspectiveFactory;
 public class GlobalTesterPerspective implements IPerspectiveFactory {
 
 	public void createInitialLayout(IPageLayout layout) {
+		String editorArea = layout.getEditorArea();
+		layout.setFixed(true);
+		
+		layout.addStandaloneView("org.globaltester.core.ui.navigator",  true, IPageLayout.LEFT, 0.3f, editorArea);
 	}
 }
---
+++ src/org/globaltester/core/ui/views/GlobalTesterNavigator.java
@@ -0,0 +1,7 @@
+package org.globaltester.core.ui.views;
+
+import org.eclipse.ui.navigator.CommonNavigator;
+
+public class GlobalTesterNavigator extends CommonNavigator {
+
+}
