ee9281af09cbb09dc9bf6e3d9c35d6cf250009e7
Alexander May <alexander.may@hjp-consulting.com>
2011-04-14 10:06:39 +0000

    add SmartCardShell UI to RCP product and make it usable in GlobalTesterPerspective
    git-svn-id: http://subversion.globaltester.org/repo/globaltester/org.globaltester.core.ui/trunk@92 cd90bb66-5c64-11e0-9e8e-9b0b1919da2d

--- src/org/globaltester/core/ui/perspective/GlobalTesterPerspective.java
+++ src/org/globaltester/core/ui/perspective/GlobalTesterPerspective.java
@@ -1,14 +1,42 @@
 package org.globaltester.core.ui.perspective;
 
+import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
+import org.eclipse.ui.IPlaceholderFolderLayout;
 
 public class GlobalTesterPerspective implements IPerspectiveFactory {
 
+	private static final String ID_FOLDER_TOPLEFT = "org.globaltester.core.ui.perspective.GlobalTesterPerspective.topLeft";
+	private static final String ID_FOLDER_BOTTOMLEFT = "org.globaltester.core.ui.perspective.GlobalTesterPerspective.bottomLeft";
+	private static final String ID_FOLDER_BOTTOM = "org.globaltester.core.ui.perspective.GlobalTesterPerspective.bottom";
+	private static final String ID_FOLDER_RIGHT = "org.globaltester.core.ui.perspective.GlobalTesterPerspective.right";
+
 	public void createInitialLayout(IPageLayout layout) {
 		String editorArea = layout.getEditorArea();
-		layout.setFixed(true);
 		
-		layout.addStandaloneView("org.globaltester.core.ui.navigator",  true, IPageLayout.LEFT, 0.3f, editorArea);
+		 // Top left: GlobalTesterNavigator view and Bookmarks view placeholder
+		 IFolderLayout topLeft = layout.createFolder(ID_FOLDER_TOPLEFT, IPageLayout.LEFT, 0.25f,
+		    editorArea);
+		 topLeft.addView("org.globaltester.core.ui.views.GlobalTesterNavigator");
+		 topLeft.addPlaceholder(IPageLayout.ID_BOOKMARKS);
+		 
+
+		 // Bottom left: Outline view and Property Sheet view
+		 IFolderLayout bottomLeft = layout.createFolder(ID_FOLDER_BOTTOMLEFT, IPageLayout.BOTTOM, 0.50f,
+		 	   ID_FOLDER_TOPLEFT);
+		 bottomLeft.addView(IPageLayout.ID_OUTLINE);
+		 
+		 // Bottom right: Task List view
+		 IFolderLayout bottom = layout.createFolder(ID_FOLDER_BOTTOM, IPageLayout.BOTTOM, 0.66f, editorArea);
+		 bottom.addView(IPageLayout.ID_PROP_SHEET);
+		 bottom.addView(IPageLayout.ID_PROGRESS_VIEW);
+		 
+		// Right: SCSH, TestLayouts etc.
+		 IPlaceholderFolderLayout right = layout.createPlaceholderFolder(ID_FOLDER_RIGHT, IPageLayout.RIGHT, 0.66f,
+	 	   editorArea);
+		 right.addPlaceholder("org.globaltester.smartcardshell.ui.views.SmartCardShellView");
+ 
+		 
 	}
 }
