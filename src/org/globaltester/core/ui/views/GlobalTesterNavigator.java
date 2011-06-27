ac9a47efd897a0fec6d45079779f0ccedf408a77
Alexander May <alexander.may@hjp-consulting.com>
2011-06-27 07:53:16 +0000

    Configure GlobalTesterNavigator to display WorkbenchResources
    git-svn-id: http://subversion.globaltester.org/repo/globaltester/org.globaltester.core.ui/trunk@156 cd90bb66-5c64-11e0-9e8e-9b0b1919da2d

--- src/org/globaltester/core/ui/views/GlobalTesterNavigator.java
+++ src/org/globaltester/core/ui/views/GlobalTesterNavigator.java
@@ -1,7 +1,13 @@
 package org.globaltester.core.ui.views;
 
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.ui.navigator.CommonNavigator;
 
 public class GlobalTesterNavigator extends CommonNavigator {
+
+	@Override
+	protected Object getInitialInput() {
+		return ResourcesPlugin.getWorkspace().getRoot();
+	}
 
 }
