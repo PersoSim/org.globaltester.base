e8247769f187c50456665e28e35c0f8f79dc1ce6
Alexander May <alexander.may@hjp-consulting.com>
2011-08-28 19:53:13 +0000

    add import feature for testspecifications
    git-svn-id: http://subversion.globaltester.org/repo/globaltester/org.globaltester.core/trunk@207 cd90bb66-5c64-11e0-9e8e-9b0b1919da2d

--- src/org/globaltester/core/resources/GtResourceHelper.java
+++ src/org/globaltester/core/resources/GtResourceHelper.java
@@ -1,34 +1,119 @@
 package org.globaltester.core.resources;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+
 import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
+import org.osgi.framework.Bundle;
 
 public class GtResourceHelper {
 	/**
 	 * create an resource together with all parents
+	 * 
 	 * @param resource
 	 * @throws CoreException
 	 */
-	public static void createWithAllParents(IResource resource) throws CoreException {
+	public static void createWithAllParents(IResource resource)
+			throws CoreException {
 		if (resource.exists()) {
 			return;
 		} else {
-			//make sure parent exists
+			// make sure parent exists
 			IContainer parent = resource.getParent();
 			createWithAllParents(parent);
-			
+
 			if (resource instanceof IFile) {
-				((IFile)resource).create(null, false, null);
+				((IFile) resource).create(null, false, null);
 			} else if (resource instanceof IFolder) {
-				((IFolder)resource).create(false, true, null);
-			}  else if (resource instanceof IProject) {
-				((IProject)resource).create(null);
-			} 
+				((IFolder) resource).create(false, true, null);
+			} else if (resource instanceof IProject) {
+				((IProject) resource).create(null);
+			}
+		}
+
+	}
+
+	/**
+	 * copy all files from an installed plugin into a new project
+	 * 
+	 * @param currentScriptPlugin
+	 * @param project
+	 */
+	public static void copyPluginContent2WorkspaceProject(String currentScriptPlugin,
+			IProject project) {
+
+		// get source path
+		Bundle curBundle = Platform.getBundle(currentScriptPlugin);
+		URL url = FileLocator.find(curBundle, new Path("/"), null);
+		IPath pluginDir = null;
+		try {
+			pluginDir = new Path(FileLocator.toFileURL(url).getPath());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		// define files to be copied
+		File source = pluginDir.toFile();
+		File destination = project.getLocation().toFile();
+		//TODO make sure that all contained/required files are copied
+		String[] children = new String[] {"TestCases"};
+		
+		//copy files
+		try {
+			for (int i = 0; i < children.length; i++) {
+				copyFiles(new File(source, children[i]), new File(
+						destination, children[i]));
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		
+		//refresh workspace
+		
+	}
+	
+	private static void copyFiles(File sourceLocation, File targetLocation)
+			throws IOException {
+
+		if (sourceLocation.isDirectory()) {
+			if (!targetLocation.exists()) {
+				targetLocation.mkdir();
+			}
+
+			String[] children = sourceLocation.list();
+			for (int i = 0; i < children.length; i++) {
+				copyFiles(new File(sourceLocation, children[i]), new File(
+						targetLocation, children[i]));
+			}
+		} else {
+
+			InputStream in = new FileInputStream(sourceLocation);
+			OutputStream out = new FileOutputStream(targetLocation);
+
+			byte[] buf = new byte[1024];
+			int len;
+			while ((len = in.read(buf)) > 0) {
+				out.write(buf, 0, len);
+			}
+			in.close();
+			out.close();
+		}
 	}
 }
