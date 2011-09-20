cc11bd2644026df1af31a14160bd7629d911100e
Alexander May <alexander.may@hjp-consulting.com>
2011-09-20 10:15:29 +0000

    add test for AboutDialog, serves as example for Menu and Button usage in SWTBot
    git-svn-id: http://subversion.globaltester.org/repo/globaltester/org.globaltester.core.ui.test/trunk@223 cd90bb66-5c64-11e0-9e8e-9b0b1919da2d

--- src/org/globaltester/core/ui/GtCoreUiTest.java
+++ src/org/globaltester/core/ui/GtCoreUiTest.java
@@ -1,20 +1,52 @@
 package org.globaltester.core.ui;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotSame;
 
 import java.io.File;
 
 import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
+import org.eclipse.swtbot.swt.finder.SWTBot;
+import org.eclipse.swtbot.swt.finder.waits.Conditions;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotButton;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
 import org.junit.Test;
 
 public class GtCoreUiTest {
-
+public static String gtMainWindowTitle = "GlobalTester";
+	
 	private SWTWorkbenchBot bot = new SWTWorkbenchBot();
 
+	/**
+	 * Test the presence of the main window and create a screenshot
+	 * @throws Exception
+	 */
 	@Test
 	public void testApplicationWindow() throws Exception {
-		Thread.sleep(5000);
+		bot.waitUntil(Conditions.shellIsActive(gtMainWindowTitle));
 		bot.captureScreenshot("screenshots"+File.separator+"MainWindow.png");
-		assertNotNull(bot.shell("GlobalTester"));
+		assertNotNull(bot.shell(gtMainWindowTitle));
+	}
+	
+	@Test
+	public void testAboutDialog() throws Exception {
+		bot.waitUntil(Conditions.shellIsActive(gtMainWindowTitle));
+		bot.menu("Help").menu("About ").click();
+		
+		String aboutShellTitle = "About ";
+		bot.waitUntil(Conditions.shellIsActive(aboutShellTitle));
+		
+		SWTBotShell aboutDlgShell = bot.shell(aboutShellTitle);
+		assertNotNull(aboutDlgShell);
+		bot.waitUntil(Conditions.shellIsActive(aboutShellTitle),5000,500);
+		bot.sleep(500);
+		bot.captureScreenshot("screenshots"+File.separator+"AboutDialog.png");
+		bot.sleep(500);
+		
+		
+		SWTBotButton okBtnBot = bot.button("OK");
+		assertNotNull(okBtnBot);
+		okBtnBot.click();
+		bot.waitUntil(Conditions.shellCloses(aboutDlgShell));
 	}
 }
