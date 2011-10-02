78caa1ca94d606e7d846e4690c876cdf4c88165b
Alexander May <alexander.may@hjp-consulting.com>
2011-10-02 20:17:13 +0000

    add Listener/Observer pattern to allow automatic update of TestCampaignTreeViewer when model changes
    git-svn-id: http://subversion.globaltester.org/repo/globaltester/org.globaltester.core/trunk@268 cd90bb66-5c64-11e0-9e8e-9b0b1919da2d

---
+++ src/org/globaltester/interfaces/ITreeChangeListener.java
@@ -0,0 +1,17 @@
+package org.globaltester.interfaces;
+
+public interface ITreeChangeListener {
+	
+	/**
+	 * Notify listeners of changed model elements
+	 *
+	 * @param notifier	object that notifies about the change
+	 * @param structureChanged true if the tree structure has changed instead of properties only
+	 * @param changedElements model elements that changed
+	 * @param properties all changed properties
+	 * 
+	 */
+	public void notifyTreeChange(Object notifier, boolean structureChanged,
+			Object[] changedElements, String[] properties);
+
+}
