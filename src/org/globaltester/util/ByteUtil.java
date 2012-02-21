d58de3af98b0ca27035df8090b850c0d2d4cac9f
Lothar Bornemann <lothar.bornemann@hjp-consulting.com>
2012-02-21 11:49:57 +0000

    incrementByte set byte value if byte Max value, set mtehod to void
    git-svn-id: http://subversion.globaltester.org/repo/globaltester/org.globaltester.core/trunk@646 cd90bb66-5c64-11e0-9e8e-9b0b1919da2d

--- src/org/globaltester/util/ByteUtil.java
+++ src/org/globaltester/util/ByteUtil.java
@@ -5,7 +5,7 @@ public class ByteUtil {
 	/*
 	 * Increment the last byte of the byte array
 	 */
-	public static byte[] incrementLastByte(byte[] bs) {
+	public void incrementLastByte(byte[] bs) {
 		
 		// get the index of the last byte in byte array
 		int index = bs.length-1;
@@ -13,14 +13,14 @@ public class ByteUtil {
 		char c = (char) bs[index]; 
 		
 		// check if lastByte = '<'
-		if (c == '<') {
-			// set to 1 if the char on this position is a checksum filler in the mrz string
+		if (c == '<' || bs[index] == 0xFF) {
+			// set to 1 if the char on this position is a checksum filler in the mrz string or byte MaxValue
 			bs[index] = 0x31;
 		} else {
 			bs[index]++;
 		}
 		
-		return bs;
+		//return bs;
 	}
 
 }
