af33f6c75b1cbab9de321e1ce79842e2406b2256
Alexander May <alexander.may@hjp-consulting.com>
2012-03-14 14:39:21 +0000

    ExecutCommand automatically use first CardConfig contained in selection (aks user if none is selected)
    git-svn-id: http://subversion.globaltester.org/repo/globaltester/org.globaltester.core.ui/trunk@816 cd90bb66-5c64-11e0-9e8e-9b0b1919da2d

--- src/org/globaltester/core/ui/GtUiHelper.java
+++ src/org/globaltester/core/ui/GtUiHelper.java
@@ -1,6 +1,12 @@
 package org.globaltester.core.ui;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
@@ -8,7 +14,7 @@ import org.eclipse.ui.ide.IDE;
 import org.globaltester.logging.logger.GtErrorLogger;
 
 public class GtUiHelper {
-	
+
 	public static void openInEditor(IFile file) {
 
 		try {
@@ -23,6 +29,37 @@ public class GtUiHelper {
 			// users most probably will ignore this behavior and open editor
 			// manually, so do not open annoying dialog
 		}
+	}
+
+	/**
+	 * Extracts all elements of the selection that are of type defined by
+	 * paramter type.
+	 * 
+	 * @param iSel
+	 *            Selection to search in
+	 * @param type
+	 *            type of expected elements
+	 * @return List containing all elements of given type, this might be empty
+	 *         but won't be null
+	 */
+	@SuppressWarnings("unchecked")
+	public static <T extends IResource> LinkedList<T> getSelectedIResource(ISelection iSel,
+			Class<T> type) {
+		LinkedList<T> selectedResources = new LinkedList<T>();
+
+		// check type of selection
+		if (iSel instanceof IStructuredSelection) {
+			Iterator<?> selectionIter = ((IStructuredSelection) iSel)
+					.iterator();
+			while (selectionIter.hasNext()) {
+				Object curElem = (Object) selectionIter.next();
+				if (type.isInstance(curElem)) {
+					selectedResources.add((T) curElem);
+				}
+			}
+		}
+
+		return selectedResources;
 	}
 
 }
