2d81ac37664fc40560430dc26101a14a6c104054
Alexander May <alexander.may@hjp-consulting.com>
2012-05-17 19:40:06 +0000

    remove redundant time format definitions in o.g.core and o.g.logging
    git-svn-id: http://subversion.globaltester.org/repo/globaltester/org.globaltester.core/trunk@984 cd90bb66-5c64-11e0-9e8e-9b0b1919da2d

--- src/org/globaltester/core/GtDateHelper.java
+++ src/org/globaltester/core/GtDateHelper.java
@@ -3,8 +3,12 @@ package org.globaltester.core;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
+import org.globaltester.logging.logger.GTLogger;
+
 public class GtDateHelper {
-	static final String gtDefaultDateFormat = "yyyyMMdd_Hmmss"; 
+	//default time format is same as default logging time format
+	static final String gtDefaultDateFormat = GTLogger.DEFAULT_DATE_FORMAT;
+	
 	
 	public static String getCurrentTimeString(){
 		SimpleDateFormat defaultSdf = new SimpleDateFormat(gtDefaultDateFormat);
