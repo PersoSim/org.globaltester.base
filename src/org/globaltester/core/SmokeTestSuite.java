3f0601bb78b6f11c963f8bc400dc0889ce2cc5fb
Martin Boonk <martin.boonk@hjp-consulting.com>
2012-08-24 09:09:50 +0000

    fix for classpath issues
    git-svn-id: http://subversion.globaltester.org/repo/globaltester/org.globaltester.core.test/trunk@1192 cd90bb66-5c64-11e0-9e8e-9b0b1919da2d

--- src/org/globaltester/core/SmokeTestSuite.java
+++ src/org/globaltester/core/SmokeTestSuite.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses(ActivatorTest.class)
+@SuiteClasses({ActivatorTest.class, ByteUtilTest.class, ExportTest.class, GtDateHelperTest.class, ZipHandlerTest.class})
 public class SmokeTestSuite {
 
 }
