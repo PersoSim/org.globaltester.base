4251028f50a84e396a159bd766d0a8f140cd3dab
Julian Koch <julian.koch@hjp-consulting.com>
2016-03-02 17:35:11 +0100

    Change toc anchor to gt base and add java project nature

---
+++ org.globaltester.base.doc/src/org/globaltester/base/doc/Activator.java
@@ -0,0 +1,25 @@
+package org.globaltester.base.doc;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+	
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+	
+	@Override
+	public void start(BundleContext context) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
