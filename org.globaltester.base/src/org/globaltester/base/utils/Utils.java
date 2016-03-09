2ff4368908f9f6a31052e9ad7f238c1d2918a1c4
Martin Boonk <martin.boonk@hjp-consulting.com>
2016-03-09 17:30:09 +0100

    Adds documentation

--- org.globaltester.base/src/org/globaltester/base/utils/Utils.java
+++ org.globaltester.base/src/org/globaltester/base/utils/Utils.java
@@ -3,8 +3,22 @@ package org.globaltester.base.utils;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
+/**
+ * This class contains generic utilities that can not usefully be bundled
+ * elsewhere.
+ * 
+ * @author mboonk
+ *
+ */
 public class Utils {
-	public static String getStacktrace(Exception exception){
+	/**
+	 * This method reads the stack trace that the given {@link Exception} would
+	 * print and returns it as a {@link String}.
+	 * 
+	 * @param exception
+	 * @return the stack trace as string
+	 */
+	public static String getStacktrace(Exception exception) {
 		ByteArrayOutputStream stacktraceBytes = new ByteArrayOutputStream();
 		exception.printStackTrace(new PrintStream(stacktraceBytes));
 		return stacktraceBytes.toString();
