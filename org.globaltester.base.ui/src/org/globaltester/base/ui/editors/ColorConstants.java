42fa57957222ad03b086445ab351315e720f2c1d
Martin Boonk <martin.boonk@hjp-consulting.com>
2016-03-24 14:06:25 +0100

    Changes colors to real constants

--- org.globaltester.base.ui/src/org/globaltester/base/ui/editors/ColorConstants.java
+++ org.globaltester.base.ui/src/org/globaltester/base/ui/editors/ColorConstants.java
@@ -3,16 +3,16 @@ package org.globaltester.base.ui.editors;
 import org.eclipse.swt.graphics.RGB;
 
 public interface ColorConstants {
-	RGB DEFAULT = new RGB(0, 0, 0);
-	RGB STRING = new RGB(0, 128, 0);
-	RGB UNKNOWN_CONTENT = new RGB(255, 0, 0);
+	final RGB DEFAULT = new RGB(0, 0, 0);
+	final RGB STRING = new RGB(0, 128, 0);
+	final RGB UNKNOWN_CONTENT = new RGB(255, 0, 0);
 
-	RGB XML_PROC_INSTR = new RGB(128, 128, 128);
-	RGB XML_TAG = new RGB(0, 128, 128);
-	RGB XML_COMMENT = new RGB(128, 0, 0);
-	RGB XML_STRING = new RGB(0, 128, 0);
+	final RGB XML_PROC_INSTR = new RGB(128, 128, 128);
+	final RGB XML_TAG = new RGB(0, 128, 128);
+	final RGB XML_COMMENT = new RGB(128, 0, 0);
+	final RGB XML_STRING = new RGB(0, 128, 0);
 
-	RGB JS_KEYWORD = new RGB(255, 0, 255);
-	RGB JS_COMMENT = new RGB(128, 0, 0);
+	final RGB JS_KEYWORD = new RGB(112, 0, 32);
+	final RGB JS_COMMENT = new RGB(128, 0, 0);
 
 }
