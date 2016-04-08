3768d10bbe0d77eaa0fea7fe576b662b527839e5
Alexander May <alexander.may@hjp-consulting.com>
2016-04-08 17:04:53 +0200

    Add Placeholder for ServiceDashboard view in GlobalTester perspective

--- org.globaltester.base.ui/src/org/globaltester/base/ui/perspective/GlobalTesterPerspective.java
+++ org.globaltester.base.ui/src/org/globaltester/base/ui/perspective/GlobalTesterPerspective.java
@@ -30,6 +30,7 @@ public class GlobalTesterPerspective implements IPerspectiveFactory {
 		 IFolderLayout bottom = layout.createFolder(ID_FOLDER_BOTTOM, IPageLayout.BOTTOM, 0.66f, editorArea);
 		 bottom.addPlaceholder(IPageLayout.ID_PROBLEM_VIEW);
 		 bottom.addPlaceholder(IPageLayout.ID_PROGRESS_VIEW);
+		 bottom.addPlaceholder("org.globaltester.service.ui.views.ServiceDashboardView");
 		 
 	}
 	
