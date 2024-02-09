15e3aebad870d9f0a4e77f1b835350ae82cfef18
Stefan Philipp <stefan.philipp@secunet.com>
2024-02-09 18:00:30 +0100

    Helper for better shutting down ExecutorService

---
+++ org.globaltester.base.test/src/org/globaltester/base/ExecutorServiceHelperTest.java
@@ -0,0 +1,40 @@
+package org.globaltester.base;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
+
+import org.junit.Test;
+
+public class ExecutorServiceHelperTest {
+
+	@Test
+	public void doTest() {
+		System.out.println("Begin: " + Instant.now());
+		ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+		List<Integer> numbers = IntStream.rangeClosed(1, 4).boxed().toList();
+		for (Integer number : numbers) {
+			executorService.submit(() -> {
+				Thread.currentThread().setName("Thread #" + number);
+				try {
+					Thread.sleep(Duration.ofSeconds(1).toMillis());
+				} catch (InterruptedException e) {
+					System.out.println("Sleep interrupted in " + Thread.currentThread().getName());
+					// Preserve interrupt status
+					Thread.currentThread().interrupt();
+					return;
+				}
+				System.out.println("Finished " + Thread.currentThread().getName() + ": " + Instant.now());
+			});
+		}
+
+		//ExecutorServiceHelper.shutdownAndAwaitTermination(
+		//	executorService, ExecutorServiceHelper.DEFAULT_WAIT_FOR_TERMINATION_DURATION, "OptionalID"); // all threads finished
+		ExecutorServiceHelper.shutdownAndAwaitTermination(executorService, Duration.ofSeconds(numbers.size() - 1L), "OptionalID"); // thread(s) interruption
+		System.out.println("End: " + Instant.now());
+	}
+}
