7e25489f0019eb5356b0c76a194cc93b9616c3af
Stefan Philipp <stefan.philipp@secunet.com>
2025-03-14 17:47:18 +0100

    GT-371: Change Bouncy Castle libraries to lateest LTS Version 2.73.7

--- org.globaltester.base/src/org/globaltester/crypto/BCProvider.java
+++ org.globaltester.base/src/org/globaltester/crypto/BCProvider.java
@@ -6,26 +6,16 @@ import org.globaltester.cryptoprovider.Crypto;
 import org.globaltester.cryptoprovider.Cryptoprovider;
 import org.osgi.framework.Constants;
 
-/**
- * This class provides legacy support for all code relying on its
- * {@link #getProvider()} method to provide a BC 1.46 {@link Cryptoprovider} for
- * compatibility issues while most other code already relies on generic JCE API
- * and also works with newer/latest BC versions or any {@link Provider} at all
- * capable of providing the required cryptographic primitives.
- * 
- * @author slutters
- * 
- */
 public class BCProvider {
 
 	private BCProvider() {
 	}
-	
+
 	public static Provider getProvider() {
 		String filterString = "(&(" + Constants.OBJECTCLASS + "=" + Cryptoprovider.class.getName() + ")" +
 		"(" + Cryptoprovider.NAME + "=" + "BC" + ")" +
-		"(" + Cryptoprovider.VERSION + "=1.7801))";
-		
+		"(" + Cryptoprovider.VERSION + "=2.7307))";
+
 		return Crypto.getCryptoProvider(filterString);
 	}
 
